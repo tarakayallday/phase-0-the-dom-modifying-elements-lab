9d54ccbeaa01e244ace776597437b4dd38f80985
Tara Kelly <tara.kelly16@gmail.com>
2021-05-26 12:35:17 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const removeMain = document.getElementById('main');
+removeMain.remove()
+
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory')
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = "Tara is the champion"
